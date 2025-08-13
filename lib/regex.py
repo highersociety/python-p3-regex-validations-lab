65f9a56516401992e4e748fccfac69e8e788ac57
trapboy01 <chriskantai95@gmail.com>
2025-08-13 19:39:03 +0300

    Name

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,9 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
-name_regex = re.compile(name)
-
-phone_number = r""
-phone_regex = re.compile(phone_number)
-
-email_address = r""
-email_regex = re.compile(email_address)
+# NAME:
+# - First and last names start with uppercase letters
+# - Allows letters, apostrophes, hyphens, and prefixes like Mc
+# - Requires exactly two words (first + last)
+name = re.compile(
+    r"^[A-Z][a-zA-Z'-]+ [A-Z][a-zA-Z'-]+$"
+)

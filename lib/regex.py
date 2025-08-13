f91e6246894330710f17dd81fb532f9c240f0d6b
trapboy01 <chriskantai95@gmail.com>
2025-08-13 20:46:10 +0300

    phonenumber

--- lib/regex.py
+++ lib/regex.py
@@ -7,3 +7,11 @@ import re
 name = re.compile(
     r"^[A-Z][a-zA-Z'-]+ [A-Z][a-zA-Z'-]+$"
 )
+
+# PHONE NUMBER:
+# - Allows optional country code (+1 or +44 etc.)
+# - Handles (123) 456-7890, 123-456-7890, 123 456 7890
+# - Allows +1-123-456-7890, +1 (123) 456-7890, etc.
+phone_number = re.compile(
+    r"^(\+\d{1,3}[- ]?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$"
+)
